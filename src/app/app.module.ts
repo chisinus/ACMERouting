5697c6abcbb3bf207177817293b6793c02e39a47
xiajun <chisinus@gmail.com>
2017-11-01 02:07:03 -0400

    1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { AuthService } from './users/auth.service';
 import { AppRoutingModule } from './app-routing.modules';
+import { WelcomeComponent } from './home/welcome.component';
+import { PathNotFoundComponent } from './home/pathnotfound.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    PathNotFoundComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
+    HttpModule,
     AppRoutingModule
   ],
   providers: [AuthService],
--- src/app/users/auth.service.ts
+++ src/app/users/auth.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
+import { IUser } from './user';
 
 @Injectable()
 export class AuthService {
+  user: IUser;
 
   constructor() { }
 
